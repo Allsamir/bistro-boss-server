556f6e470f71b574320cc75536a37503429c6809
Al Samir <shahinur1591@gmail.com>
2024-05-20 22:14:21 +0600

    cors setup done

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ import mongoose from "mongoose";
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 mongoose
   .connect(process.env.DATABASE_URI)
