e606876f70866bad5fe7523e23df42a480c60a8e
Al Samir <shahinur1591@gmail.com>
2024-05-21 22:56:30 +0600

    add to cart api done

--- apis/api.js
+++ apis/api.js
@@ -1,6 +1,7 @@
 import express, { Router } from "express";
 import Menu from "../models/menu.js";
 import Review from "../models/review.js";
+import Cart from "../models/cart.js";
 const router = Router();
 //Menu Collection
 router.get("/menus", async (req, res) => {
@@ -23,7 +24,33 @@ router.get("/reviews", async (req, res) => {
 //Carts Collection
 router.post("/carts", async (req, res) => {
   try {
-  } catch (err) {}
+    const { email, cartItems } = req.body;
+    const userEmailFromDB = await Cart.find({ email: email });
+    if (userEmailFromDB.length === 0) {
+      const newCart = new Cart({
+        email: email,
+        cartItems: [cartItems],
+      });
+      await newCart.save().then(() => {
+        res.status(200).send({ message: "Food added to your cart" });
+      });
+    } else {
+      await Cart.findOneAndUpdate(
+        { email: email },
+        {
+          $push: {
+            cartItems: cartItems,
+          },
+        },
+        {
+          new: true,
+        },
+      );
+      res.status(200).send({ message: "Food added to your cart" });
+    }
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 export default router;
--- models/cart.js
+++ models/cart.js
@@ -2,4 +2,9 @@ import mongoose from "mongoose";
 
 const cartSchema = mongoose.Schema({
   email: String,
+  cartItems: [String],
 });
+
+const Cart = mongoose.model("Cart", cartSchema);
+
+export default Cart;
